da4b71385b92daf1067bceded37ca0ad8ce5128c
Fiona Gill <C18470754@mytudublin.ie>
2022-04-26 00:13:23 +0100

    Builder pattern for alert dialog

--- app/src/main/java/com/example/assignment4/AdminHome.java
+++ app/src/main/java/com/example/assignment4/AdminHome.java
@@ -2,6 +2,8 @@ package com.example.assignment4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -52,7 +54,33 @@ public class AdminHome extends AppCompatActivity {
         return true;
     }
 
-    public void logOut(MenuItem menuItem){
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_Logout) {
+            showPopup();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showPopup() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(AdminHome.this);
+        alert.setMessage("Are you sure?")
+                .setPositiveButton("Logout", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        logOut();
+
+                    }
+                }).setNegativeButton("Cancel", null);
+
+        AlertDialog alert1 = alert.create();
+        alert1.show();
+    }
+
+    public void logOut(){
         FirebaseAuth.getInstance().signOut();
         Intent i = new Intent(AdminHome.this, MainActivity.class);
         startActivity(i);
--- app/src/main/java/com/example/assignment4/Home.java
+++ app/src/main/java/com/example/assignment4/Home.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -22,13 +24,12 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
 
-
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNav);
         Menu menu = bottomNavigationView.getMenu();
         MenuItem menuItem = menu.getItem(0);
         menuItem.setChecked(true);
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
-            switch (item.getItemId()){
+            switch (item.getItemId()) {
                 case R.id.navigation_home:
                     break;
 
@@ -43,7 +44,7 @@ public class Home extends AppCompatActivity {
                     break;
 
                 case R.id.navigation_bag:
-                    Intent intent3= new Intent(Home.this, ShoppingCart.class); // need to create recycler class
+                    Intent intent3 = new Intent(Home.this, ShoppingCart.class); // need to create recycler class
                     startActivity(intent3);
                     break;
 
@@ -58,7 +59,34 @@ public class Home extends AppCompatActivity {
         return true;
     }
 
-    public void logOut(MenuItem menuItem){
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_Logout) {
+
+            showPopup();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showPopup() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(Home.this);
+        alert.setMessage("Are you sure?")
+                .setPositiveButton("Logout", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        logOut();
+
+                    }
+                }).setNegativeButton("Cancel", null);
+
+        AlertDialog alert1 = alert.create();
+        alert1.show();
+    }
+
+    public void logOut() {
         FirebaseAuth.getInstance().signOut();
         Intent i = new Intent(Home.this, MainActivity.class);
         startActivity(i);
